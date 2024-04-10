cf6fd35edf14ac1b05af0f3b2be50e1d123023bb
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-10 20:16:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let value1 = prompt("Enter the first value.");
 let value2 = prompt("Enter the second value.");
 
 // Try to convert the inputs to numbers
-value1 = isNaN(Number(value1)) ? value1 : Number(value1);
-value2 = isNaN(Number(value2)) ? value2 : Number(value2);
+value1 = isNaN(value1) ? value1 : Number(value1);
+value2 = isNaN(value2) ? value2 : Number(value2);
 
 alert(isSameType(value1, value2));
