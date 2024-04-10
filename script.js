e10dc594e67ba517a25cf8e06600faff73a673db
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-10 20:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 function isSameType(value1, value2) {
   if (isNaN(value1) && isNaN(value2)) {
     return true;
-	 
+  } else if (isNaN(value1) || isNaN(value2)) {
+    return false;
   }
-   return typeof value1 === typeof value2;
-
+  return typeof value1 === typeof value2;
 }
-
-let value1 = prompt("Enter the first value.");
-let value2 = prompt("Enter the second value.");
-
-// Try to convert the inputs to numbers
-value1 = isNaN(value1) ? value1 : Number(value1);
-value2 = isNaN(value2) ? value2 : Number(value2);
-
-alert(isSameType(value1, value2));
